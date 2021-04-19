29724d5ff66877d73d140f9adad4b31af145f7d9
Evan Schoening <evan.schoening@gmail.com>
2021-04-19 05:08:07 -0700

    Added index and website handler

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use server::Server;
 use http::Request;
 use http::Method;
 use std::net::TcpListener;
+// pulling below in for environment variables
+use std::env;
 use website_handler::WebsiteHandler;
 
 mod server;
@@ -11,11 +13,13 @@ mod http;
 mod website_handler;
 
 fn main() {
-
-
-
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+    // environment variable for path
+    // created public directory for public path
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
+    println!("public path : {}", public_path);
     let server = Server::new("127.0.0.1:8080".to_string());
-    server.run(WebsiteHandler);
+    server.run(WebsiteHandler::new(public_path));
 }
 
 // Everything inside of moduleis private by default.
--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,13 +1,31 @@
 use super::server::Handler;
 use super::http::{Request, Response, StatusCode, Method};
+use std::fs;
 
-pub struct WebsiteHandler;
+pub struct WebsiteHandler {
+    // defining public paths for internet access
+    public_path : String
+}
+
+//
+impl WebsiteHandler {
+    pub fn new(public_path: String) -> Self {
+        Self { public_path }
+    }
+
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+        // need to convert this result into an option
+        // Use Ok option in this case to convert
+        fs::read_to_string(path).ok()
+    }
+}
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response{
         match request.method() {
             Method::GET => match request.path() {
-                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())),
+                "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
                 "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello</h1>".to_string())),
                 _ => Response::new(StatusCode::NotFound, None),
             }
